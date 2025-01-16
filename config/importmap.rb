63db6a028a9c049e7214cd80004c89758fe217f7
ayako-k2 <a.kiyonari@gmail.com>
2025-01-16 11:16:34 +0900

    Changed importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,3 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -11,3 +9,20 @@ pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
 pin "timediff", to: "timediff.js"
 pin "time_display", to: "time_display.js"
+
+
+
+
+# Pin npm packages by running ./bin/importmap
+
+# pin "application"
+# pin "@hotwired/turbo-rails", to: "turbo.min.js"
+# pin "@hotwired/stimulus", to: "stimulus.min.js"
+# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+# pin_all_from "app/javascript/controllers", under: "controllers"
+# pin "place", to: "place.js"
+# pin "type_to_category_map", to: "type_to_category_map.js"
+# pin "@rails/actioncable", to: "actioncable.esm.js"
+# pin_all_from "app/javascript/channels", under: "channels"
+# pin "timediff", to: "timediff.js"
+# pin "time_display", to: "time_display.js"
